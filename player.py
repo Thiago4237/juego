012583bf7baef77d004372154e62135fede6de2c
veronica <v.naranjo@utp.edu.co>
2025-05-17 18:36:12 -0500

    Agregar las teclas A S W D para mover al personaje

--- player.py
+++ player.py
@@ -1,7 +1,6 @@
 import pygame
 import os
 import random
-from os.path import join
 from settings import *
 from battery import Battery  # Importar la clase Battery
 
@@ -10,8 +9,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.load_images()
         self.state, self.frame_index = 'down', 0
-        current_dir = os.path.dirname(__file__)
-        self.image = pygame.image.load(join(current_dir, 'Resources', 'img', 'player', 'down', '0.png')).convert_alpha()        
+        self.image = pygame.image.load(join('Resources', 'img', 'player', 'down', '0.png')).convert_alpha()        
         self.rect = self.image.get_rect(center=position)
         self.hitbox_rect = self.rect.inflate(-60, -60)
         self.direction = pygame.Vector2()
@@ -25,9 +23,8 @@ class Player(pygame.sprite.Sprite):
     
     def load_images(self):
         self.frames = {'left': [], 'right': [], 'up': [], 'down': []}
-        for state in self.frames.keys():
-            current_dir = os.path.dirname(__file__)
-            for folder_path, sub_folders, file_names in walk(os.path.join(current_dir, 'Resources', 'img', 'player', state)):
+        for state in self.frames.keys():            
+            for folder_path, sub_folders, file_names in walk(join('Resources', 'img', 'player', state)):
                 if file_names:
                     for file_name in sorted(file_names, key=lambda x: int(x.split('.')[0])):                   
                         full_path = os.path.join(folder_path, file_name)
@@ -36,8 +33,8 @@ class Player(pygame.sprite.Sprite):
     
     def input(self):
         keys = pygame.key.get_pressed()
-        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
+        self.direction.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
         self.direction = self.direction.normalize() if self.direction else self.direction 
         
     def move(self, dt):
@@ -62,7 +59,9 @@ class Player(pygame.sprite.Sprite):
             self.state = 'right' if self.direction.x > 0 else 'left'
         if self.direction.y != 0:
             self.state = 'down' if self.direction.y > 0 else 'up'
-        self.frame_index += 5 * dt
+            
+        #animate
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0 
         self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
     
     def update_light(self, dt):
