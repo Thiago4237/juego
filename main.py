4e60232ccc4f3f4e0c266b709c1eb1f558d25640
veronica <v.naranjo@utp.edu.co>
2025-05-23 21:12:02 -0500

    Revert "Agregar segundo personaje y modificación del sonido"
    This reverts commit a7f48f28ea5a357d9e9566762f01bc9195fb9269.

--- main.py
+++ main.py
@@ -27,9 +27,6 @@ class Game:
         self.pause_selected_option = 0
         self.pause_options = ['Continuar', 'Reiniciar', 'Menú Principal']
         
-        # Selección de personaje
-        self.selected_character = "veronica"
-        
         # Grupos de sprites (se inicializan vacíos, se llenan en setup)
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
@@ -51,14 +48,14 @@ class Game:
         # Temporizador para aparición de enemigos
         self.enemy_event = pygame.event.custom_type()
         pygame.time.set_timer(self.enemy_event, 300)
-        self.spawn_positions = []    
+        self.spawn_positions = []
         
         # Cargar sonidos y música
         self.shoot_sound = pygame.mixer.Sound(join('audio', 'shoot.wav'))
         self.shoot_sound.set_volume(0.4)
         self.impact_sound = pygame.mixer.Sound(join('audio', 'impact.ogg'))
-        self.music = pygame.mixer.Sound(join('audio', 'principal.mp3'))
-        self.music.set_volume(1)
+        self.music = pygame.mixer.Sound(join('audio', 'music.wav'))
+        self.music.set_volume(0.3)
         self.music.play(loops=-1)
         
         # Sistema de puntaje
@@ -185,7 +182,7 @@ class Game:
         player_pos = None
         for obj in map.get_layer_by_name('Entities'):
             if obj.name == 'Player':
-                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites, self.battery_sprites, character=self.selected_character)
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites, self.battery_sprites)
                 self.gun = Gun(self.player, self.all_sprites)
                 player_pos = (obj.x, obj.y)
             else:
--- menu.py
+++ menu.py
@@ -12,19 +12,6 @@ class Menu:
         self.show_help = False
         self.input_name_active = False
         self.player_name = ""
-        self.character_selection_active = False
-        self.characters = ['veronica', 'santiago']  
-        self.selected_character = 0  
-
-        # Cargar la imagen de fondo para la selección de personajes
-        self.char_selection_bg = pygame.image.load(join('Resources', 'img', 'Personaje.png')).convert_alpha()
-        self.char_selection_bg = pygame.transform.scale(self.char_selection_bg, (WINDOW_WIDTH, WINDOW_HEIGHT))
-
-        # Definir las áreas de los personajes
-        self.char_areas = [
-            pygame.Rect(350, 600, 250, 450),  # veronica (left character)
-            pygame.Rect(700, 600, 250, 450)   # Santiago (right character)
-        ]
 
     def draw_menu(self):
         self.display_surface.fill('black')
@@ -72,19 +59,6 @@ class Menu:
         instruction_text = self.font.render("Presiona ENTER para continuar", True, (255, 255, 255))
         self.display_surface.blit(instruction_text, (WINDOW_WIDTH // 2 - 100, WINDOW_HEIGHT // 2 + 50))
 
-    def draw_character_selection(self):
-        
-        self.display_surface.blit(self.char_selection_bg, (0, 0))
-        
-        triangle_size = 20
-        selected_rect = self.char_areas[self.selected_character]
-        triangle_points = [
-            (selected_rect.centerx - 10, selected_rect.top),
-            (selected_rect.centerx + 10, selected_rect.top),
-            (selected_rect.centerx, selected_rect.top - triangle_size)
-        ]
-        pygame.draw.polygon(self.display_surface, (255, 255, 0), triangle_points)
-
     def handle_input_name(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -94,8 +68,7 @@ class Menu:
                 if event.key == pygame.K_RETURN and self.player_name.strip():
                     self.game.player_name = self.player_name.strip()
                     self.input_name_active = False
-                    self.character_selection_active = True
-                    return None
+                    return True
                 elif event.key == pygame.K_BACKSPACE:
                     self.player_name = self.player_name[:-1]
                 elif event.key == pygame.K_ESCAPE:
@@ -105,31 +78,10 @@ class Menu:
                     self.player_name += event.unicode
         return None
 
-    def handle_character_selection(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                    self.selected_character = (self.selected_character - 1) % len(self.characters)
-                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                    self.selected_character = (self.selected_character + 1) % len(self.characters)
-                elif event.key == pygame.K_RETURN:
-                    self.game.selected_character = self.characters[self.selected_character]
-                    self.character_selection_active = False
-                    return True
-                elif event.key == pygame.K_ESCAPE:
-                    self.character_selection_active = False
-                    self.player_name = ""
-                    return None
-        return None
-
     def run(self, start_with_main_menu=True):
         self.show_scores = False
         self.show_help = False
         self.input_name_active = False
-        self.character_selection_active = False
         self.player_name = ""
         if not start_with_main_menu:
             self.input_name_active = True
@@ -138,18 +90,10 @@ class Menu:
                 result = self.handle_input_name()
                 if result is not None:
                     if result:
-                        return True
+                        return True  # Iniciar juego
                     else:
-                        return False
+                        return False  # Salir
                 self.draw_name_input()
-            elif self.character_selection_active:
-                result = self.handle_character_selection()
-                if result is not None:
-                    if result:
-                        return True
-                    else:
-                        continue
-                self.draw_character_selection()
             elif self.show_scores:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
